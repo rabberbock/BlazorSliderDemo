d4a3e69243d7490de3f15d8398d27fb4b9bb9ff5
Raffi Abberbock <rabberbock@gmail.com>
2021-08-30 20:32:01 -0400

    code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorSlider;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAntDesign();
+
+await builder.Build().RunAsync();
